d044d5411f6c49cf1a389fabb915e27c3a23e276
Showrove Roy <showrove200@gmail.com>
2022-10-28 15:54:32 +0600

    subject api url create

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 5000;
 const courses = require("./data/courses.json");
 const level = require("./data/level.json");
+const subject = require("./data/subject.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -12,6 +13,11 @@ app.get("/", (req, res) => {
 // get all level api
 app.get("/level", (req, res) => {
   res.send(level);
+});
+
+// create subject api url
+app.get("/subject", (req, res) => {
+  res.send(subject);
 });
 
 // get all courses Api
