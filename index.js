9877b9a960e719fa9e101e0c3a8dcccb14ea5d44
Showrove Roy <showrove200@gmail.com>
2022-10-28 18:24:59 +0600

    api routes conflict solve

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const courses = require("./data/courses.json");
 const level = require("./data/level.json");
 const subject = require("./data/subject.json");
@@ -21,14 +21,14 @@ app.get("/subject", (req, res) => {
 });
 
 // get single subject
-app.get("/courses/:subject", (req, res) => {
+app.get("/subject/:subject", (req, res) => {
   const sub = req.params.subject;
   const selectedSubject = courses.filter((course) => course.subject === sub);
   res.send(selectedSubject);
 });
 
 // get single subject
-app.get("/courses/:level", (req, res) => {
+app.get("/level/:level", (req, res) => {
   const levl = req.params.level;
   const selectedlevel = courses.filter((course) => course.level === levl);
   res.send(selectedlevel);
