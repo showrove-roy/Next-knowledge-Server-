110ff16218cc58aac4fbbc404dd5c476c8259f8e
Showrove Roy <showrove200@gmail.com>
2022-10-28 14:07:27 +0600

    categories api url create

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 const courses = require("./data/courses.json");
+const categories = require("./data/categories.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Next knowledge Server Running ");
 });
+// get all categories api
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+// get all courses Api
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
